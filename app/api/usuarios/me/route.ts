7f109610b82f513c4897022435acd7b676e13c41
thiagodeassisbm <thiago.deassisbm@gmail.com>
2026-01-21 16:52:58 -0300

    fix: explicit dynamic on API and clients to fix 502

--- app/api/usuarios/me/route.ts
+++ app/api/usuarios/me/route.ts
@@ -1,5 +1,6 @@
-
 import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic';
 import { PrismaClient } from '@prisma/client';
 import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
