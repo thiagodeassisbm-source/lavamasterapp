4fc155c27fa7885751616cb57f1d8bf70648e514
thiagodeassisbm <thiago.deassisbm@gmail.com>
2026-01-22 12:12:53 -0300

    chore: remove debug routes and temporary scripts for security

--- generate-update-sql.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs')
-
-async function generateHash() {
-    const hash = await bcrypt.hash('admin123', 10)
-    console.log(`UPDATE super_admins SET senha = '${hash}' WHERE email = 'thiago.deassisbm@gmail.com';`)
-}
-
-generateHash()
--- update-password.sql
+++
@@ -1,8 +0,0 @@
--- Atualizar senha do Super Admin para 'admin123'
--- Hash gerado recentemente
-UPDATE super_admins 
-SET senha = '$2a$10$IiAPYr0FSqQt.rvJe/LulnmCa' 
-WHERE email = 'thiago.deassisbm@gmail.com';
-
--- Verificar se o usuário existe e exibir
-SELECT id, nome, email, ativo FROM super_admins WHERE email = 'thiago.deassisbm@gmail.com';
